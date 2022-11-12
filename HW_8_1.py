7dbbb82c74793c570e07432e04b2bfd97fdbece3
Лариса Сергеева <larisa.sergeeva@mail.ru>
2022-11-12 23:30:01 +0300

    Домашнее задание к лекции "Работа с библиотекой requests, http-запросы". Задаса №1.

--- HW_8_1.py
+++ HW_8_1.py
@@ -14,17 +14,15 @@ def max_intillegence_superhero_name(url_hero, name_hero_list):
     intillegence_hero = {}
     if connect_url(url_hero):
         response = requests.get(url_hero).json()
-        print(response)
         for _ in range(len(response)):
             for name in name_hero_list:
                 if response[_]['name'] == name:
                     intillegence_hero[name] = response[_]['powerstats']['intelligence']
         super_superhero = max(intillegence_hero.items(), key=lambda k_v: k_v[1])
-        print(super_superhero[0])
         return super_superhero[0]
 
 
 superhero_name_list = ['Hulk', 'Captain America', 'Thanos']
 url = "https://akabab.github.io/superhero-api/api/all.json"
-max_intillegence_superhero_name(url, superhero_name_list)
+print(max_intillegence_superhero_name(url, superhero_name_list))
 
